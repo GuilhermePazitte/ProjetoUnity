55f57441cfbb3ab098aa4173c8fd18408079eb58
Guilherme Pazitte <guipazitte@gmail.com>
2024-05-17 22:16:14 -0300

    Inclusao Inimigo

---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    public GameObject tiroPrefab;
+    public float forceSpeedFire;
+    public bool isPlayer;
+    private float currentTime;
+    public float shootInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= shootInterval)
+        {
+            currentTime = 0;
+            Atirar();
+        }
+    }
+
+    public void Atirar()
+    {
+        GameObject tempTiro = Instantiate(tiroPrefab) as GameObject;
+        tempTiro.transform.position = transform.position;
+
+        tempTiro.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceSpeedFire));
+    }
+}
